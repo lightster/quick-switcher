1625f349e9600357824e94f5a9f09792e1fbb280
Matt Light <matt.light@lightdatasys.com>
2016-12-09 23:39:55 -0800

    Add ugly initial mock layout

---
+++ src/quick-switcher.js
@@ -0,0 +1,11 @@
+(function() {
+  $('.lstr-qswitcher-search').on('keydown', function () {
+    console.log('down')
+  })
+  $('.lstr-qswitcher-search').on('keyup', function () {
+    console.log('up')
+  })
+  $('.lstr-qswitcher-search').on('keypress', function () {
+    console.log('press')
+  })
+})();
