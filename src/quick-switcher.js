e3615e5a9abf8940cd96261dcfe98193b18f5f00
Matt Light <matt.light@lightdatasys.com>
2016-12-11 23:57:41 -0800

    Generate list DOM dynamically in JS

--- src/quick-switcher.js
+++ src/quick-switcher.js
@@ -9,3 +9,28 @@
     console.log('press')
   })
 })();
+
+(function($ ) {
+  var methods = {
+    renderList: function(list) {
+      var $ul = $('<ul>');
+      this.append($ul);
+
+      $(list).each(function(idx, value) {
+        var $li = $('<li>');
+        $ul.append($li);
+        $li.text(value);
+      })
+    }
+  };
+
+  $.fn.lstrQuickSwitcher = function(methodOrOptions) {
+    if (methods[methodOrOptions]) {
+      return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' +  methodOrOptions + ' does not exist on jQuery.lstrQuickSwitcher');
+    }
+  };
+}(jQuery));
