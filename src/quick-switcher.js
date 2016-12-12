042bce5127ad836a45096668eddcb9ed07a392c4
Matt Light <matt.light@lightdatasys.com>
2016-12-12 00:44:54 -0800

    Move entirety of DOM generation to JS
    The DOM should not need to be manually added to
    the DOM before the JS is ran.

--- src/quick-switcher.js
+++ src/quick-switcher.js
@@ -10,27 +10,70 @@
   })
 })();
 
-(function($ ) {
-  var methods = {
-    renderList: function(list) {
-      var $ul = $('<ul>');
-      this.append($ul);
+if (typeof module === 'undefined') {
+  module = {};
+}
 
-      $(list).each(function(idx, value) {
+if (typeof module.exports === 'undefined') {
+  module.exports = {};
+}
+
+if (typeof module.exports.jQuery === 'undefined') {
+  module.exports.jQuery = jQuery;
+}
+
+(function(exports) {
+  var QuickSwitcher = {
+    init: function($parentDom, options) {
+      this.initDomElement($parentDom);
+      this.options = options;
+
+      this.renderList();
+    },
+
+    initDomElement: function($parentDom) {
+      this.$domElement = $(
+        '<div class="lstr-qswitcher-container">' +
+        '  <form class="lstr-qswitcher-popup">' +
+        '    <span class="lstr-qswitcher-help">' +
+        '      <ul>' +
+        '        <li>↵ to navigate</li>' +
+        '        <li>↵ to select</li>' +
+        '        <li>↵ to clear</li>' +
+        '        <li>↵ to dismiss</li>' +
+        '      </ul>' +
+        '    </span>' +
+        '    <span class="lstr-qswitcher-category">' +
+        '      People' +
+        '    </span>' +
+        '    <input type="text" class="lstr-qswitcher-search" />' +
+        '    <div class="lstr-qswitcher-results">' +
+        '    </div>' +
+        '  </form>' +
+        '</div>'
+      );
+
+      $parentDom.append(this.$domElement);
+    },
+
+    renderList: function() {
+      var $results = this.$domElement.find('.lstr-qswitcher-results');
+      var $ul = $('<ul>');
+
+      this.options.forEach(function(value) {
         var $li = $('<li>');
         $ul.append($li);
         $li.text(value);
       })
+
+      $results.append($ul);
     }
   };
 
-  $.fn.lstrQuickSwitcher = function(methodOrOptions) {
-    if (methods[methodOrOptions]) {
-      return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
-    } else if (typeof methodOrOptions === 'object' || !methodOrOptions) {
-      return methods.init.apply(this, arguments);
-    } else {
-      $.error('Method ' +  methodOrOptions + ' does not exist on jQuery.lstrQuickSwitcher');
-    }
+  exports.lstrQuickSwitcher = function(options) {
+    var $parentDom = $('body');
+
+    quickSwitcher = Object.create(QuickSwitcher);
+    quickSwitcher.init($parentDom, options);
   };
-}(jQuery));
+}(module.exports));
