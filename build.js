197bde31810fd8f56161f3056787d9d6b363ec33
Matt Light <matt.light@lightdatasys.com>
2017-01-17 23:56:07 -0800

    Reconfigure require to use 'src' as baseUrl
    This way quick-switcher can be broken down into
    smaller pieces without requiring all dependencies
    needing to be referenced via 'src/'...

--- build.js
+++ build.js
@@ -1,8 +1,7 @@
 {
-  baseUrl: ".",
-  name: "src/quick-switcher",
-  name: 'node_modules/almond/almond',
-  include: ['src/quick-switcher'],
+  baseUrl: "src",
+  name: '../node_modules/almond/almond',
+  include: ['quick-switcher'],
   out: "dist/quick-switcher.min.js",
   wrap: {
     startFile: '.almond/start.frag',
