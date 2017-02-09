d2512e824a5c41925c82529ee0c2ac57e4d58bc6
Matt Light <matt.light@lightdatasys.com>
2017-02-08 23:44:42 -0800

    Add tape+faucet and start testing
    Test filters.js

---
+++ test/filters.js
@@ -0,0 +1,30 @@
+var requirejs = require('requirejs');
+var test = require('tape');
+
+requirejs.config({
+  baseUrl: 'src',
+});
+
+test('simple substring can be found', function(t) {
+  t.plan(1);
+
+  var filters = requirejs('filters');
+
+  t.assert(filters.isMatch('world', 'hello world and friends'));
+});
+
+test('multi-word search criteria can be matched', function(t) {
+  t.plan(1);
+
+  var filters = requirejs('filters');
+
+  t.assert(filters.isMatch('world friends', 'hello world and friends'));
+});
+
+test('all search words must be in search index to be a match', function(t) {
+  t.plan(1);
+
+  var filters = requirejs('filters');
+
+  t.assert(!filters.isMatch('world friends the', 'hello world and friends'));
+});
