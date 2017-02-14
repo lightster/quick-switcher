f236983acb3832d2f368d78a77271a907604f6cb
Matt Light <matt.light@lightdatasys.com>
2017-02-13 23:03:50 -0800

    Move scorer to Selection prototype
    The scorer is going to depend on more selection
    data, so I am moving Selection to its own class and
    including the ability to score the Selection directly

--- src/tracker.js
+++ src/tracker.js
@@ -1,29 +1,4 @@
-define('tracker', [], function() {
-  var time = function() {
-    return Math.floor(new Date().getTime() / 1000);
-  };
-  var Selection = {
-    init: function(data) {
-      if (data) {
-        this.timestamps = data.timestamps;
-        this.count = data.count;
-        return;
-      }
-
-      this.timestamps = [];
-      this.count = 0;
-    },
-
-    increment: function() {
-      this.timestamps.push(time());
-      ++this.count;
-
-      while (this.timestamps.length > 10) {
-        this.timestamps.shift();
-      }
-    },
-  };
-
+define('tracker', ['tracker/selection'], function(Selection) {
   return {
     init: function(trackerName) {
       this.name = trackerName;
@@ -104,49 +79,11 @@ define('tracker', [], function() {
     scoreSelection: function(item) {
       var selection = this.selections[item.trackerId];
 
-      if (typeof selection === 'undefined' || !selection.timestamps.length) {
+      if (typeof selection === 'undefined') {
         return 0;
       }
 
-      var now = time();
-
-      /**
-       * Scoring based on weights and calculation used by Slack as
-       * described in their article:
-       * https://slack.engineering/a-faster-smarter-quick-switcher-77cbc193cb60#.cb5ofyxyl
-       */
-      var score = selection.timestamps.reduce(
-        function(score, timestamp) {
-          if (timestamp > now - (3600 * 4)) {
-            return score + 100;
-          }
-
-          if (timestamp > now - (3600 * 24)) {
-            return score + 80;
-          }
-
-          if (timestamp > now - (3600 * 24 * 3)) {
-            return score + 60;
-          }
-
-          if (timestamp > now - (3600 * 7)) {
-            return score + 40;
-          }
-
-          if (timestamp > now - (3600 * 30)) {
-            return score + 20;
-          }
-
-          if (timestamp > now - (3600 * 90)) {
-            return score + 10;
-          }
-
-          return score;
-        },
-        0
-      );
-
-      return selection.count * score / selection.timestamps.length;
+      return selection.score();
     },
   };
 });
