cf49a1b62978e52c622c6b7929dc7993c15f33fd
Matt Light <matt.light@lightdatasys.com>
2017-03-10 00:19:28 -0800

    Split docs JS into separate files
    I am going to add some more samples, so it is best to split
    things out now

--- docs/index.js
+++ docs/index.js
@@ -1,65 +1,23 @@
 require.config({
-  baseUrl: 'src'
+  baseUrl: 'src',
+  paths: {
+    'docs': '../docs',
+  },
 });
 
-define(['quick-switcher'], function(lstrQuickSwitcher) {
+define(function(require) {
+  var lstrQuickSwitcher = require('quick-switcher');
+
   lstrQuickSwitcher({
     searchCallback: function(searchText, basicResultHandler) {
-      basicResultHandler.setResults(basicResultHandler.sorters.tracker('main').sort([
-        {
-          breadcrumbText: 'Peoplez',
-          text: 'people',
-          trackerId: 'People',
-          description: {html: '&#128269;'},
-          searchCallback: function searchCallback(searchText, peopleResultHandler) {
-            setTimeout(function () {
-              peopleResultHandler.setResults(peopleResultHandler.sorters.tracker('person').sort([
-                'lightster',
-                'zulu',
-                'ollie',
-                'enderwiggin',
-                'twenty7',
-                'netbattler',
-                'majorstriker',
-                'nimble',
-                'darthsidious',
-                'pariah',
-                'blackstar',
-                'boni'
-              ].filter(function(item) {
-                var text = item;
-                if (item.text) {
-                  text = item.text;
-                } else if (item.html) {
-                  text = item.html;
-                }
-                return peopleResultHandler.filters.isMatch(searchText.toLowerCase(), text.toLowerCase());
-              }).map(function (item) {
-                return {
-                  text: item,
-                  trackerId: item
-                };
-              }), searchText));
-            }, 1);
+      basicResultHandler.setResults(
+        basicResultHandler.sorters.tracker('main').sort([
+          require('docs/search/people'),
+          require('docs/search/error'),
+          {
+            text: 'Quickest Item',
+            trackerId: 'Quickest Item',
           },
-          searchDelay: 500,
-          selectCallback: function selectCallback(selected) {
-            console.log(selected.selectedValue);
-            selected.trackAs('person');
-          }
-        },
-        {
-          breadcrumbText: 'Demo Error',
-          text: 'Demo Error',
-          trackerId: 'Demo Error',
-          searchCallback: function searchCallback(searchText, errorResultHandler) {
-            errorResultHandler.setError();
-          }
-        },
-        {
-          text: 'Quickest Item',
-          trackerId: 'Quickest Item'
-        }
       ].filter(function(item) {
         return basicResultHandler.filters.isMatch(searchText, item.text);
       }), searchText));
